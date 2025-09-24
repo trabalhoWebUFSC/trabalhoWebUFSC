ac8d2050702b37470be9eff2d2c1c26b638ea2f5
joaopaulodecker <joaopaulodecker@gmail.com>
2025-09-24 20:49:44 -0300

    Configuracao inicial: estrutura MERN e regras do .gitignore

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      {/* O SEU MÍNIMO "HELLO WORLD" */}
+      <h1>Portal do Hóspede - Hello World!</h1>
+      <p>O Front-end está rodando com sucesso.</p>
+    </div>
+  );
+}
+
+export default App;
