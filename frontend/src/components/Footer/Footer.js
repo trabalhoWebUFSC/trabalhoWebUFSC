b3261d5cd24ba39c3bb3a5b505249a4e3feb71fb
joaopaulodecker <joaopaulodecker@gmail.com>
2025-10-23 15:01:02 -0300

    feat(footer) e fix(responsividade e estilos)

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import logo from "../../assets/images/logo-hotel.png";
+
+// Importar ícones
+import { FaFacebookF, FaInstagram, FaEnvelope } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        {/* Coluna 2 - Logo, Texto, etc*/}
+        <div className={styles.footerCol}>
+          <img src={logo} alt="Imperial Grand Hotel" className={styles.logo} />
+          <p className={styles.description}>
+            Com lindos apartamentos, o Imperial Grand Hotel tornou-se um ícone
+            de charme e elegância ao Sul do Brasil, na Ilha de Santa Catarina,
+            oferecendo um ambiente acolhedor com uma vista privilegiada.
+          </p>
+          <hr className={styles.divider} />
+          <div className={styles.legalContainer}>
+            <p className={styles.copyright}>
+              © 2025 Imperial Grand Hotel. Todos os direitos reservados.
+            </p>
+            <div className={styles.legalLinks}>
+              <a href="/privacy">Política de Privacidade</a>
+              <a href="/terms">Termos de Uso</a>
+            </div>
+          </div>
+        </div>
+
+        {/* Coluna 2 -  Links e Redes sociais */}
+        <div className={styles.footerCol}>
+          <ul className={styles.linksList}>
+            <li>
+              <a href="/">INÍCIO</a>
+            </li>
+            <li>
+              <a href="/about">SOBRE NÓS</a>
+            </li>
+            <li>
+              <a href="/services">SERVIÇOS</a>
+            </li>
+            <li>
+              <a href="/rooms">QUARTOS</a>
+            </li>
+          </ul>
+          <div className={styles.socialIcons}>
+            <a
+              href="https://www.facebook.com/majesticpalacehotel/?locale=pt_BR"
+              aria-label="Facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.instagram.com/majesticpalacehotel/"
+              aria-label="Instagram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="mailto:contato@imperialgrandhotel.com"
+              aria-label="Email"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaEnvelope />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroHotel from "../../components/HeroHotel/HeroHotel";
+import Footer from "../../components/Footer/Footer";
 
 function Home() {
   return (
     <>
       <HeroHotel />
+      <Footer />
     </>
   );
 }
