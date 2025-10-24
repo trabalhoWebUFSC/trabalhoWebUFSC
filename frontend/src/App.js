7aba0824d221b2008ea197267c9295ef1037bd8b
Nicole Cristina <nicolecristina.uv@gmail.com>
2025-10-24 16:03:58 -0300

    Add NavBar component to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar /> 
       <Home />
     </div>
   );
