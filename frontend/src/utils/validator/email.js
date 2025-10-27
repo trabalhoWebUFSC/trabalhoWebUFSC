3d2cdfe12ae3e61c17ca212f92d9c28015605da2
Sofia Damas <sofiadamvs@gmail.com>
2025-10-27 11:54:50 -0300

    feat: adiciona pasta de funções utilitárias

---
+++ frontend/src/utils/validator/email.js
@@ -0,0 +1,7 @@
+const validateEmail = (value) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  if (!emailRegex.test(value)) return "E-mail inválido"
+  return ''
+}
+
+export { validateEmail };
