00682e75cc4e0bebfcf5e22b74babac1dcf11ab8
Nicole Cristina <nicolecristina.uv@gmail.com>
2025-11-02 20:50:07 -0300

    Add PromoBanner component with promotional content

---
+++ frontend/src/components/PromoBannerr/PromoBanner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./PromoBanner.module.css";
+import promoBannerImage from '../../assets/images/promobanner.png';
+import promoBannerImage2 from '../../assets/images/promobanner2.png';
+
+
+
+
+function PromoBanner() {
+  return (
+    <section className={styles.promoBanner}>
+      <div className={styles.promoContent}>
+        <div className={styles.promoImages}>
+         <img src={promoBannerImage} alt="Promo" className={styles.promoImage} />
+         <img src={promoBannerImage2} alt="Promo" className={styles.promoImage} />
+        </div> 
+         <div className={styles.promoText}>
+          <h1 className={styles.promoH1}>Get A Luxurious Experience With 25% Off Now</h1>
+          <button className={styles.promoButton}>Book Now &gt; </button>
+          </div>
+      </div>
+    </section>
+  );
+}
+
+export default PromoBanner;
