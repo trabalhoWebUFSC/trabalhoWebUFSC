8a7691156bf77ab12878d61eddbe6f77dbcf185c
Sofia Damas <sofiadamvs@gmail.com>
2025-11-10 01:18:31 -0300

    fix: ajusta routing do portal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
+import AboutPage from "./pages/About/About.js";
 import LoginPage from "./pages/Login/Login";
 import RegisterPage from "./pages/Register/Register";
-import AboutPage from "./pages/About/About.js";
-
-import Navbar from "./components/NavBar/NavBar";
+import PortalPage from "./pages/Portal/Portal"
 import Footer from "./components/Footer/Footer";
-
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -18,6 +17,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/portal" element={<PortalPage />} />
       </Routes>
 
       <Footer />
