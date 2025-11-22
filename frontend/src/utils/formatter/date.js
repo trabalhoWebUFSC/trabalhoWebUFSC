39e27136a60dde7d597aecdde00ba3b8bbc49934
Sofia Damas <sofiadamvs@gmail.com>
2025-11-22 01:55:23 -0300

    fix: ajusta documentação de date

--- frontend/src/utils/formatter/date.js
+++ frontend/src/utils/formatter/date.js
@@ -1,25 +1,18 @@
-/**
- * Formata uma data string ISO (YYYY-MM-DD ou YYYY-MM-DDTHH:mm:ss)
- * para o formato brasileiro (DD/MM/YYYY).
- * * @param {string} dateString - Data vinda do backend
- * @returns {string} Data formatada
- */
 export const formatBRLDate = (dateString) => {
   if (!dateString) return '';
 
   try {
-    // 1. Garante que pegamos apenas a parte da data, ignorando a hora (depois do T)
-    // Ex: "2025-11-27T00:00:00.000Z" vira apenas "2025-11-27"
+    // ignora a hora (depois do T)
     const cleanDate = dateString.split('T')[0];
 
-    // 2. Separa ano, mês e dia
+    // separa ano, mês e dia
     const [year, month, day] = cleanDate.split('-');
 
-    // 3. Retorna montado: Dia / Mês / Ano
+    // retorna formatado para XX/XX/XXXX
     return `${day}/${month}/${year}`;
     
   } catch (error) {
-    console.error("Erro ao formatar data:", error);
-    return dateString; // Retorna original em caso de erro
+    console.error("Error formatting date:", error);
+    return dateString;
   }
 };
