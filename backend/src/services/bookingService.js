3f83ed81c5930ae9e177824bdf632aa14cf61c87
ViniciusAnverze <viniverze@gmail.com>
2025-11-24 23:43:13 -0300

    fix: validacao data checkin e checkout booking

--- backend/src/services/bookingService.js
+++ backend/src/services/bookingService.js
@@ -4,11 +4,19 @@ const Room = require('../models/Room');
 
 const createBooking = async (user, bookingData) => {
   const { roomId, checkIn, checkOut, people, guestEmail } = bookingData;
-  
+
   // Validacao do quarto
   const room = await Room.findById(roomId);
   if (!room) throw new Error('Quarto não encontrado');
   if (people > room.capacity) throw new Error('Capacidade do quarto excedida');
+
+  if (
+    new Date(checkIn) < new Date(new Date().setHours(0, 0, 0, 0)) ||
+    new Date(checkOut) < new Date(new Date().setHours(0, 0, 0, 0)) ||
+    new Date(checkOut) <= new Date(checkIn)
+  ) {
+    throw new Error('Datas inválidas: check-in ou check-out antes de hoje, ou check-out menor/igual ao check-in.');
+  }
 
   // Valida as datas
   const existingBooking = await Booking.findOne({
@@ -24,11 +32,11 @@ const createBooking = async (user, bookingData) => {
   if (existingBooking) {
     throw new Error('Datas indisponíveis para este quarto.');
   }
-  
+
   // Calcula o preço
   const nights = (new Date(checkOut) - new Date(checkIn)) / (1000 * 60 * 60 * 24);
   const totalPrice = nights * room.pricePerNight;
-  
+
   // Processa pagamento
   // const payment = await paymentService.processPayment(user._id, totalPrice);
 
@@ -42,9 +50,9 @@ const createBooking = async (user, bookingData) => {
     totalPrice,
     status: 'confirmed', // Mude para 'pending' se precisar de pagamento
     // payment: payment._id,
-    
+
     reservedByEmail: user.email,
-    companionEmail: guestEmail || null, 
+    companionEmail: guestEmail || null,
   });
 
   await newBooking.save();
@@ -58,8 +66,8 @@ const getMyReservationsForUser = async (userEmail) => {
       { companionEmail: userEmail }
     ]
   })
-  .populate('room', 'name photos') // Traz nome e fotos do quarto
-  .sort({ checkIn: -1 }); // Ordena por data (mais novas primeiro)
+    .populate('room', 'name photos') // Traz nome e fotos do quarto
+    .sort({ checkIn: -1 }); // Ordena por data (mais novas primeiro)
 
   return reservations;
 };
